dccc5d1fb55268540b36bec81cd0350d8d37b51c
viettu2k <doanviettu2k@gmail.com>
2022-08-19 09:47:52 +0700

    implementing html

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import "./App.css";
-
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="kirby">
+      <div className="arm arm-left"></div>
+      <div className="arm arm-right"></div>
+      <div className="foot foot-left"></div>
+      <div className="foot foot-right"></div>
+      <div className="mouth"></div>
+      <div className="eye eye-left">
+        <div className="inner"></div>
+      </div>
+      <div className="eye eye-right">
+        <div className="inner"></div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
