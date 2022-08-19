40bcef270e81dcdb2a01f0c884d4a4d2e38d97eb
viettu2k <doanviettu2k@gmail.com>
2022-08-19 10:30:46 +0700

    sytiing mouth and fix layout

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 function App() {
   return (
-    <div className="kirby">
-      <div className="arm arm-left"></div>
-      <div className="arm arm-right"></div>
-      <div className="foot foot-left"></div>
-      <div className="foot foot-right"></div>
-      <div className="mouth"></div>
-      <div className="eye eye-left">
-        <div className="inner"></div>
-      </div>
-      <div className="eye eye-right">
-        <div className="inner"></div>
+    <div className="App">
+      <div className="kirby">
+        <div className="arm arm-left"></div>
+        <div className="arm arm-right"></div>
+        <div className="foot foot-left"></div>
+        <div className="foot foot-right"></div>
+        <div className="mouth"></div>
+        <div className="eye eye-left">
+          <div className="inner"></div>
+        </div>
+        <div className="eye eye-right">
+          <div className="inner"></div>
+        </div>
       </div>
     </div>
   );
